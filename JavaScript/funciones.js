976ab236be75373c6d3733731668331b63552d8c
arturochaconrios <achaconrios@gmail.com>
2024-05-29 11:32:37 -0400

    actualizac

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -1,7 +1,7 @@
 function obtenerFormularioJquery(){
 
 //alert("accion desde el Formulario")
-var Nombre = $("#nombre").val()
+var nombre = $("#nombre").val()
 
 if(nombre.length<=1){
   errores = errores + "<p>Nombre es campo requerido o debe ser mayor a un caracter</p>"  
