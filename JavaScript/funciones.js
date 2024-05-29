613e56b8538624ed5a6b1b7682e91617ccca2193
arturochaconrios <achaconrios@gmail.com>
2024-05-29 12:35:37 -0400

    a

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -2,8 +2,20 @@ function obtenerFormularioJquery(){
 
 //alert("accion desde el Formulario")
 var nombre = $("#nombre").val()
+var nombre = $("#cuadro").val()
+var nombre = $("#precio").val()
+var nombre = $("#fecha").val()
 
 if(nombre.length<=1){
+  errores = errores + "Nombre es campo requerido o debe ser mayor a un caracter"  
+}
+if(cuadro.length<=0){
+  errores = errores + "Nombre es campo requerido o debe ser mayor a un caracter"  
+}
+if(precio.length<=0){
+  errores = errores + "Nombre es campo requerido o debe ser mayor a un caracter"  
+}
+if(fecha.length<=0){
   errores = errores + "Nombre es campo requerido o debe ser mayor a un caracter"  
 }
 if(errores !=""){
