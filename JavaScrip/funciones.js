8ac839d4dfc3a85a6dfeb14b971c82ff44f1a408
arturochaconrios <achaconrios@gmail.com>
2024-05-28 23:43:49 -0400

    cambio

--- JavaScrip/funciones.js
+++ JavaScrip/funciones.js
@@ -1,9 +1,9 @@
 function obtenerFormulario(){
 
 //alert("accion desde el Formulario")
-var Nombre = document.getElementById(idnombre).value
+var Nombre = document.getElementById("nombre").value
 
-if(nombre.length<=1)[
+if(nombre.length<=1){
   errores=errores + "Nombre es campo requerido o debe ser mayor a un caracter\n"  
-]
+}
 }
