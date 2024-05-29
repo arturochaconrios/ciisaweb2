689ddb3f352e17a7b6026220592ae9d300c7e36d
arturochaconrios <achaconrios@gmail.com>
2024-05-29 00:51:03 -0400

    cambio total de contacto

--- JavaScrip/funciones.js
+++ JavaScrip/funciones.js
@@ -1,9 +1,13 @@
-function obtenerFormulario(){
+function obtenerFormularioJquery(){
 
 //alert("accion desde el Formulario")
-var Nombre = document.getElementById("nombre").value
+var Nombre = $("#nombre").val()
 
 if(nombre.length<=1){
-  errores = errores + "Nombre es campo requerido o debe ser mayor a un caracter\n"  
+  errores = errores + "<p>Nombre es campo requerido o debe ser mayor a un caracter</p>"  
+}
+if(errores !=""){
+    $("#errores").html(errores)
+    return false
 }
 }
