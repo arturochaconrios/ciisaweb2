c6afc20a6d70b31071e1377a4fbeb4e85eca3ec8
arturochaconrios <achaconrios@gmail.com>
2024-06-02 18:56:34 -0400

    jhjk

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,28 +1,26 @@
-$(document).ready(function(){
-    //var datatable="",
-                $.ajax({
-                    url:'https://huachitos.cl/api/animales',
-                    dataType:'jason',
-                    success:function(data){
-    
-                        var galerias=data.data
-   
-                new datatable('#galerias1',{     
-                                                    
-                    "data": galerias,
-                    "columns": [
-                      {"data": 'Artista'},
-                      {"data": 'Nombre'}, 
-                      {"data": 'Tipo'},
-                      {"data": 'Medidas'},
-                    ],
-                    loyout:{
-                      topStart:{
-                        button:['copyhtmnl5', 'excelhtml5','csvhtml5','pdfhtml5']
-                      }
-                    }
-                  })
-                    }
-                  })
-                  })
-                  
+              
+
+                  $(document).ready(function(){
+                    $.ajax({
+                        url:'https://huachitos.cl/api/animales',
+                        datatype:'json',
+                        success:function(data){
+                            var galeria = data.data
+                
+                           new DataTable('#galerias1', {
+                                "data": galeria,
+                                "columns": [
+                                    {"data" : 'Artista'},
+                                    {"data" : 'Tipo'},
+                                    {"data" : 'Genero'},
+                                ],
+                                layout: {
+                                    topStart: {
+                                        buttons: ['copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5']
+                                    }
+                                }
+                            
+                            })
+                         } 
+                    })
+                })
