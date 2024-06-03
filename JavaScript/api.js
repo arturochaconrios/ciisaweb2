87f5fd96c771d5685504faaf2a257c993ef56200
arturochaconrios <achaconrios@gmail.com>
2024-06-02 22:45:04 -0400

    as

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -2,7 +2,7 @@
 
                   $(document).ready(function(){
                     $.ajax({
-                        url:'https://huachitos.cl/api/animales',
+                        url:'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number',
                         datatype:'JSON',
                         success:function(data){
                             var galeria = data.data
@@ -10,10 +10,10 @@
                           new DataTable('#galerias1', {
                                 "data": galeria,
                                 "columns": [
-                                    {"data" : 'Imagen'},
-                                    {"data" : 'Nombre'},
-                                    {"data" : 'Edad'},
-                                    {"data" : 'Estado'},
+                                    {"data" : 'Titulo'},
+                                    {"data" : 'Artista'},
+                                    {"data" : 'Fecha'},
+                                    {"data" : 'Referencia'},
                                 ],
                                 layout: {
                                     topStart: {
