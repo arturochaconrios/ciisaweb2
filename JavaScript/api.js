c7871c74fbfd47bdf8639902c9dab4d54a3b5df3
arturochaconrios <achaconrios@gmail.com>
2024-06-02 23:38:55 -0400

    final

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,29 +1,29 @@
               
 
-                  $(document).ready(function(){
-                    $.ajax({
-                        url:'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number',
-                        datatype:'JSON',
-                        success:function(data){
-                            var galeria = data.data
+               //   $(document).ready(function(){
+                 //   $.ajax({
+                   //     url:'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number',
+                     //   datatype:'JSON',
+                       // success:function(data){
+                         //   var galeria = data.data
                 
-                          new DataTable('#galerias1', {
-                                "data": galeria,
-                                "columns": [
-                                    {"data" : 'Titulo'},
-                                    {"data" : 'Artista'},
-                                    {"data" : 'Fecha'},
-                                    {"data" : 'Referencia'},
+                        //  new DataTable('#galerias1', {
+                          //      "data": galeria,
+                            //    "columns": [
+                              //      {"data" : 'Titulo'},
+                                //    {"data" : 'Artista'},
+                                  //  {"data" : 'Fecha'},
+                                    //{"data" : 'Referencia'},
                                 ],
-                                layout: {
-                                    topStart: {
-                                        buttons: ['copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5']
-                                    }
-                                }
-                            
-                            })
-                         } 
-                    })
-                })
+                  //              layout: {
+                    //                topStart: {
+                      //                  buttons: ['copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5']
+                        //            }
+                          //      }
+             //               //
+               //             })
+                 //        } 
+                   // })
+               // })
 
               
